b71862a1f7dbf9b17010bc158160b90d999be1c4
shunan_k <shunan_k@163.com>
2022-01-07 09:48:50 +0800

    third commit

--- platinum.c
+++ platinum.c
@@ -19,12 +19,26 @@ int main(void){
     printf("You are easily worth that! If platinum prices drop,\n");
     printf("eat more to maintain your value.\n");
 
+
     printf("----------------------------------------\n");
 
+    /* 以十进制，八进制，十六进制打印十进制数100*/
     int x = 100;
 
     printf("dec = %d; octal = %o; hex = %x\n", x, x, x);
     printf("dex = %d; octal = %#o; hex = %#x\n", x, x, x);
+
+
+    printf("----------------------------------------\n");
+
+    /* 超出系统允许的最大int值*/
+    int i = 2147483647;
+    unsigned int j = 4294967295;
+
+    printf("%d, %d, %d", i, i + 1, i + 2);
+    printf("%u, %u, %u\n", j, j + 1, j + 2);
+
+
 
 
     return 0;
